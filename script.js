bc891e21139afe34ea761096fb5d9d843678aa6d
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-04 22:02:43 -0700

    just committing the progress...

--- script.js
+++ script.js
@@ -20,21 +20,63 @@ function showError(message) {
 //*******Part 2 & 3 ************ */
 console.log(`*****Part 2 & 3****`);
 //General Requirements & Registration Form Validation Requirements
-
+//Username
 const usernameInput = document.querySelector('input[name = username]');
+const emailInput = document.querySelector(`input[name="email"]`);
+const passwordInput = document.querySelector('input[name = "password"]');
+const passwordCheckInput = document.querySelector('input[name="passwordCheck"]');
 
-usernameInput.addEventListener('input', () => {
+
+usernameInput.addEventListener('input', (e) => {
+    console.log(usernameInput.value.length);
     if (usernameInput.value.length < 4) {
+        console.log(usernameInput.value.length)
         usernameInput.setCustomValidity('Username must be at least 4 characters long.');
     }
 })
+//Email
+emailInput.addEventListener('input', (e) => {
 
-const emailInput = document.querySelector(`input[name="email"]`);
-emailInput.addEventListener('input', () => {
     if (emailInput.validity.valid && emailInput.value.endsWith('@example.com')) {
         showError(`Email addresses from example.com are not allowed.`);
     }
 });
+//Password
+document.getElementById('registration').addEventListener('submit', () => {
+    // let validFlag = true;
+    if (passwordInput.value.toLowerCase().includes(`password`)) {
+        showError(`Passwords cannot contain the word "password".`);
+        // validFlag = false;
+    }
+    if (passwordInput.value.includes(usernameInput.value)) {
+        showError(`Passwords cannot contain the username.`);
+        //validFlag = false;
+    }
+
+    passwordCheckInput.addEventListener('input', () => {
+        if (passwordCheckInput.value !== passwordInput.value) {
+            showError(`Passwords must match.`);
+        }
+        //validFlag = false;
+        //if (!valid) {
+        //    event.preventDefault();
+        //}
+    })
+});
+
+
+
+//passwordInput.addEventListener('input', () => {
+//if(passwordInput.value !== passwordCheckInput.value){
+//    showError(`Passwords must match.`);
+//}
+//})
+//if (passwordCheckInput.value.toLowerCase().includes(`password`)) {
+//    showError(`Passwords cannot contain the word "password".`);
+//}
+//if (passwordCheckInput.value.includes(usernameInput.value)) {
+//    showError(`Passwords cannot contain the username.`);
+//}
 
 
 
