2522452c1c586702f5686dbedb722c5611b61571
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-04 19:27:07 -0700

    part one: error messsage display

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log(`ALAB Form Validation`);
+
+//*******Part 1: Introduction************ */
+console.log(`*****Part 1: Introduction****`);
+
+const errDisplay = document.getElementById('errorDisplay');
+
+function showError(message) {
+    errDisplay.textContent = message; // Set the error message
+    errDisplay.style.display = 'flex'; // Show the error display
+    setTimeout(() => {
+        errDisplay.style.display = 'none'; // Hide after 3 seconds
+    }, 3000);
+}
+
+showError("This is an error message.");
+
+
+
